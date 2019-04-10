800128b90ce7e30ad60285adc6567e2be834ca80
kenshinpg <xinran@biosan.cn>
2019-04-10 19:19:36 +0800

    update

--- projects/Biosan-service-upgrade-2019/src/config.py
+++ projects/Biosan-service-upgrade-2019/src/config.py
@@ -9,6 +9,6 @@ class Config(object):
 
 	def __init__(self):
 
-		self.origin_data_file = 'C:\\Jupyter\\博圣工作\\其他服务项\\维修项目\\service\\data\\2015-2018维修数据_部分.xlsx'
+		self.origin_data_filepath = 'C:\\Jupyter\\博圣工作\\其他服务项\\维修项目\\service\\data\\'
 		self.savefig_path = '../source/fig/'
 		os.makedirs(self.savefig_path, exist_ok=True)
