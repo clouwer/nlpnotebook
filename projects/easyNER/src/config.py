882d4ba3520b931a652c3c68554844e607ac7b4f
kenshinpg <xinran@biosan.cn>
2019-04-22 22:54:41 +0800

    update

---
+++ projects/easyNER/src/config.py
@@ -0,0 +1,14 @@
+
+import json
+import os
+import time
+
+class Config(object):
+
+	"""Docstring for Config. """
+
+	def __init__(self):
+
+		self.origin_datapath = '../source/data/origin_data/'
+		self.userdict = '../source/data/ner_data/ner_pos.txt'
+
